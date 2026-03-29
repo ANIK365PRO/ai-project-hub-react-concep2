af6d78b37601e729ddb6fb348dbf56cd3dc1f016
Anik Mohanta <mohonto05@gmail.com>
2026-03-29 15:24:11 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// 1. function, method, component, hooks
+// 2. props
+// 3. parent -> child.
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <section id="center">
+        
+          <h1 className='text-5xl'>Hallo</h1>
+         
+      </section>
+
+    </>
+  )
+}
+
+export default App

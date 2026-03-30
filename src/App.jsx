c18e10702745810972d79e4135d1fd3518466b70
Anik Mohanta <mohonto05@gmail.com>
2026-03-30 10:04:02 +0600

    added daisy-Tab in App component > get useState, toggle Tab,and {active === 'Models'? Tab1 : Tab2} for show component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@
 // 3. parent -> child.
 
 
+import { useState } from 'react'
 import './App.css'
 import Banner from './Components/Banner/Banner'
 import Footer from './Components/Footer/Footer'
 import Models from './Components/Models/Models'
 import NavBar from './Components/NavBar/NavBar'
+import Cart from './Components/Cart/Cart'
 
 
 const getModels = async () =>{
@@ -17,14 +19,38 @@ const getModels = async () =>{
 const modelPromise = getModels()
 
 function App() {
-
+  const [activeTab, setActiveTab] = useState('Models')
+  const handleActiveTab = (tab) => {
+    setActiveTab(tab)
+  }
 
   return (
     <>
       <NavBar></NavBar>
       <Banner></Banner>
         
-       <Models modelPromise={modelPromise}></Models>
+      {/* name of each tab group should be unique */}
+      <div className="tabs tabs-box justify-center bg-transparent mt-5">
+        <input 
+          type="radio" 
+          name="my_tabs_1" 
+          className="tab w-40 rounded-2xl text-xl font-semibold" 
+          aria-label="Models"
+          onClick={() => handleActiveTab('Models')} 
+          defaultChecked />
+        <input 
+          type="radio" 
+          name="my_tabs_1" 
+          className="tab w-40 rounded-2xl text-xl font-semibold" 
+          aria-label="Cart"
+          onClick={() => handleActiveTab('Cart')}  />
+          
+      </div>
+
+
+      {activeTab === 'Models' &&  <Models modelPromise={modelPromise}></Models>}
+     {activeTab === 'Cart' && <Cart></Cart>}
+
       <Footer></Footer>
          
     
---
+++ src/Components/Cart/Cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cart = () => {
+    return (
+        <div className='p-20 bg-linear-to-r from-red-400 via-orange-400 to-amber-400'>
+            <h1 className='text-5xl font-black'>cart section</h1>
+        </div>
+    );
+};
+
+export default Cart;
